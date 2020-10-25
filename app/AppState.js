791b772091cbd5d47e81feadb35dbc6fc443e53b
milesfwilson <milesfwilson@gmail.com>
2020-10-24 23:07:30 -0600

    Subtle changes to links

--- app/AppState.js
+++ app/AppState.js
@@ -1,6 +1,7 @@
 import Todo from "./Models/Todo.js"
 import Weather from "./Models/Weather.js"
-import Quote from './models/Quote.js'
+import Quote from './Models/Quote.js'
+
 import { EventEmitter } from "./Utils/EventEmitter.js"
 import { isValidProp } from "./Utils/isValidProp.js"
 
